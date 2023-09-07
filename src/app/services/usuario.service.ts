ae8e6b824e1b7d207db967f509f3c5790734d604
FirminoAndrade <firmino_andrade@yahoo.com.br>
2023-09-07 09:46:10 -0300

    atualizacao de acesso a api

--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -4,7 +4,7 @@ import { Observable } from 'rxjs';
 import { IUsuario } from '../models/usuario.modelo';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
-const url = 'http://localhost:8081/api/';
+const url = 'http://localhost:8080/api/';
 
 @Injectable({
   providedIn: 'root',
