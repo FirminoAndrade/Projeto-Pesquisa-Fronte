927dc8fb68138002ac5c0ec543ab155077d16902
FirminoAndrade <firmino_andrade@yahoo.com.br>
2023-09-23 13:59:20 -0300

    atualizacao

--- server.js
+++ server.js
@@ -1,17 +1,10 @@
 const express = require("express");
-const path = require("path");
-
 const app = express();
+const appName = 'projeto-pesquisa';
+const outputpath = `${__dirname}/dist/${appName}`;
 
-// Serve os arquivos estáticos da pasta dist (gerada pelo ng build)
-app.use(express.static(__dirname + "/dist/projeto-pesquisa"));
-
-app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/projeto-pesquisa/index.html"));
+app.use(express.static(outputpath));
+app.get('/*', (req,res) => {
+  res.sendFile(`${outputpath}/index.html`);
 });
-
-// Inicia a aplicação pela porta configurada
-app.listen(process.env.PORT || 8080);
-
-
-/*npm install express path --save*/
+app.listen(process.env.PORT);
