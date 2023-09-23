854cb4438b3b546ce7faac8f0aa5c3970bbbdbf6
FirminoAndrade <firmino_andrade@yahoo.com.br>
2023-09-23 15:20:09 -0300

    add no banco

--- src/app/components/views/login/login.component.ts
+++ src/app/components/views/login/login.component.ts
@@ -20,6 +20,9 @@ export class LoginComponent implements OnInit {
 
   ngOnInit(): void {}
 
+  a() {
+    this.router.navigate(['/home']);
+  }
   submitLogin() {
     this.service.validarSenha(this.email, this.senha).subscribe({
       next: (resposta) => {
